d479c6981701ac0e4557b4123536d9c7e7551914
alexsevas <mralexrelax@gmail.com>
2024-09-21 06:23:35 +0300

    Add whisper-openai example

---
+++ STT_Whisper/openai_whisper_example1/transcribe_audio_to_txt.py
@@ -0,0 +1,33 @@
+# Developed by MikyPo
+# More code for DA here: https://dzen.ru/mikypo
+
+# conda activate all2py310 (в PyCharm запускает процесс на CPU)
+# ENV all2py310, extras
+# pip install git+https://github.com/openai/whisper.git --quiet
+# pip install ffmpeg --quiet
+
+import os
+import whisper
+
+# Установим путь к модели в переменную окружения
+os.environ['WHISPER_CACHE_DIR'] = 'C:/AI/Whisper'
+language = 'ru'
+model_path = 'C:/AI/Whisper/large-v3.pt'
+
+# Загружаем модель
+if os.path.exists(model_path):
+    model = whisper.load_model(model_path)
+else:
+    model = whisper.load_model('large')  # Или пусть скачает из интернета
+    
+options = whisper.DecodingOptions(language=language)
+
+# Загружаем аудио файл
+audio = whisper.load_audio('1.wav')
+audio = whisper.pad_or_trim(audio)
+
+# Выполняем распознавание
+result = model.transcribe(audio)
+
+# Выводим результат
+print(result['text'])
