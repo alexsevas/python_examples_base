c95e79d5c0d8a2619b6bbe6bb53262ad3562ff09
alexsevas <mralexrelax@gmail.com>
2024-09-29 16:59:27 +0300

    Add p2day parsing les02: from index.html to json

--- Parsing/p2day/lesson2.py
+++ Parsing/p2day/lesson2.py
@@ -1,9 +1,11 @@
-#pip install beautifulsoup4 requests lxml
+# pip install beautifulsoup4 requests lxml
 
 import requests
 from bs4 import BeautifulSoup
+import json
 
-#парсим страницу и сохраняем ее в index.html
+# парсим страницу и сохраняем ее в index.html
+'''
 url = "http://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
 headers = {
     "Accept": "*/*",
@@ -13,5 +15,22 @@ req = requests.get(url, headers=headers)
 src = req.text
 #print(src)
 
-with open("index.html", "w", encoding='utf-8') as file:
+with open("index.html", "w", encoding="utf-8") as file:
     file.write(src)
+'''
+
+# загружаем данные из index.html и работаем дальше с ним
+with open("index.html", encoding="utf-8") as file:
+    src = file.read()
+
+soup = BeautifulSoup(src, "lxml")
+all_products_hrefs = soup.find_all(class_="mzr-tc-group-item-href")
+
+all_categories_dict = {}
+for item in all_products_hrefs:
+    item_text = item.text
+    item_href = "http://health-diet.ru" + item.get("href")
+
+    all_categories_dict[item_text] = item_href
+with open("all_categories_dict.json", "w", encoding="utf-8") as file:
+    json.dump(all_categories_dict, file, indent=4, ensure_ascii=False)
