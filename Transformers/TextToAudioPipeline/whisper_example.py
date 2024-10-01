b325ea020a257c4d253c9b25b823cbaba539ef56
alexsevas <mralexrelax@gmail.com>
2024-10-01 07:07:32 +0300

    Add: no translate version

--- Transformers/TextToAudioPipeline/whisper_example.py
+++ Transformers/TextToAudioPipeline/whisper_example.py
@@ -10,9 +10,10 @@ device = 0 if torch.cuda.is_available() else "cpu"
 file = "D:\\AI\\p1.wav"
 #file = "D:\\AI\\ПОДКАСТЫ\\Podlodka #102 - Многопоточность.mp3"
 
-# используемая модель хранится тут в кэше: C:\Users\A43X\.cache\huggingface\hub\models--openai--whisper-large-v2\
+#используемая модель хранится тут в кэше: C:\Users\A43X\.cache\huggingface\hub\models--openai--whisper-large-v2\
 pipe = pipeline(task="automatic-speech-recognition", model="openai/whisper-large-v2", chunk_length_s=30, device=device)
-text = pipe(file, batch_size=8, generate_kwargs={"task": "translate"}, return_timestamps=True)["text"]
+text = pipe(file, batch_size=8, return_timestamps=True)["text"] #original
+#text = pipe(file, batch_size=8, generate_kwargs={"task": "translate"},return_timestamps=True)["text"] #to ENG
 #print(text)
 
 with open("result.txt", "w", encoding="utf-8") as file:
