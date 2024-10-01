2be4c9494dd3c670d39633cfc4e10999986fd159
alexsevas <mralexrelax@gmail.com>
2024-10-01 06:56:39 +0300

    Add: model to GPU, translate to ENG, save result.txt

--- Transformers/TextToAudioPipeline/whisper_example.py
+++ Transformers/TextToAudioPipeline/whisper_example.py
@@ -3,7 +3,22 @@
 # pip install transformers
 
 from transformers import pipeline
-transcriber = pipeline(model="openai/whisper-large-v2")
-#transcriber("https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/1.flac")
-result = transcriber("D:\AI\p1.wav")
-print(result)
+
+import torch
+
+device = 0 if torch.cuda.is_available() else "cpu"
+file = "D:\\AI\\p1.wav"
+#file = "D:\\AI\\ПОДКАСТЫ\\Podlodka #102 - Многопоточность.mp3"
+
+# используемая модель хранится тут в кэше: C:\Users\A43X\.cache\huggingface\hub\models--openai--whisper-large-v2\
+pipe = pipeline(task="automatic-speech-recognition", model="openai/whisper-large-v2", chunk_length_s=30, device=device)
+text = pipe(file, batch_size=8, generate_kwargs={"task": "translate"}, return_timestamps=True)["text"]
+#print(text)
+
+with open("result.txt", "w", encoding="utf-8") as file:
+    file.write(text)
+
+del pipe
+torch.cuda.empty_cache()
+
+
