f406a33629630672f1bf3d188f76ed2b69525f31
alexsevas <mralexrelax@gmail.com>
2024-10-02 19:06:43 +0300

    Add final Jarvis_ver01

--- AI/Voice_Assistants/Jarvis_ver01/app02.py
+++ AI/Voice_Assistants/Jarvis_ver01/app02.py
@@ -1,14 +1,23 @@
 # conda activate jarvis
-# чат с llama3-70b-8192 через API groq, "зрение" через API Gemini, все общение в консоли
+# final version
 
 from groq import Groq
 from PIL import ImageGrab, Image
+from openai import OpenAI
+from faster_whisper import WhisperModel
+import speech_recognition as sr
 import google.generativeai as genai
 import pyperclip
 import cv2
+import pyaudio
+import os
+import time
+import re
 
+wake_word = 'jarvis'
 groq_client = Groq(api_key="YOUR KEY")
 genai.configure(api_key="YOUR KEY")
+openai_client = OpenAI(api_key='YOUR KEY')
 web_cam = cv2.VideoCapture(0)
 
 sys_msg = (
@@ -42,6 +51,18 @@ model = genai.GenerativeModel('gemini-1.5-flash-latest',
                               safety_settings=safety_settings
                               )
 
+num_cores = os.cpu_count()
+whisper_size = 'base'
+whisper_model = WhisperModel(
+    whisper_size,
+    device='cpu',
+    compute_type='int8',
+    cpu_threads=num_cores // 2,
+    num_workers=num_cores //2
+)
+
+r = sr.Recognizer()
+source = sr.Microphone()
 
 def groq_prompt(prompt, img_context):
     if img_context:
@@ -105,6 +126,83 @@ def vision_prompt(prompt, photo_path):
     response = model.generate_content([prompt, img])
     return response.text
 
+def speak(text):
+    player_stream = pyaudio.PyAudio().open(format=pyaudio.paInt16, channels=1, rate=24000, output=True)
+    stream_start = False
+
+    with openai_client.audio.speech.with_streaming_response.create(
+        model='tts-1',
+        voice='onyx',
+        response_format='pcm',
+        input=text,
+    ) as response:
+        silence_threshold = 0.01
+        for chunk in response.iter_bytes(chunk_size=1024):
+            if stream_start:
+                player_stream.write(chunk)
+            else:
+                if max(chunk) > silence_threshold:
+                    player_stream.write(chunk)
+                    stream_start = True
+
+def wav_to_text(audio_path):
+    segments, _ = whisper_model.transcribe(audio_path)
+    text = ''.join(segment.text for segment in segments)
+    return text
+
+def callback(recognizer, audio):
+    prompt_audio_path = 'prompt.wav'
+    with open(prompt_audio_path, 'wb') as f:
+        f.write(audio.get_wav_data())
+
+    prompt_text = wav_to_text(prompt_audio_path)
+    clean_prompt = extract_prompt(prompt_text, wake_word)
+
+    if clean_prompt:
+        print(f'USER: {clean_prompt}')
+        call = function_call(clean_prompt)
+        if 'take screenshot' in call:
+            print('Taking screenshot')
+            take_screenshot()
+            visual_context = vision_prompt(prompt=clean_prompt, photo_path='screenshot.jpg')
+        elif 'capture webcam' in call:
+            print('Capturing webcam')
+            web_cam_capture()
+            visual_context = vision_prompt(prompt=clean_prompt, photo_path='webcam.jpg')
+        elif 'extract clipboard' in call:
+            print('Copying clipboard text')
+            paste = get_clipboard_text()
+            prompt = f'{clean_prompt}\n\n CLIPBOARD CONTENT: {paste}'
+            visual_context = None
+        else:
+            visual_context = None
+
+        response = groq_prompt(prompt=clean_prompt, img_context=visual_context)
+        print(f'ASSISTANT: {response}')
+        speak(response)
+
+def start_listening():
+    with source as s:
+        r.adjust_for_ambient_noise(s, duration=2)
+    print('\nSay ', wake_word, 'followed with your prompt. \n')
+    r.listen_in_background(source, callback)
+
+    while True:
+        time.sleep(.5)
+
+def extract_prompt(transcribed_text, wake_word):
+    pattern = rf'\b{re.escape(wake_word)}[\s,.?!]*([A-Za-z0-9].*)'
+    match = re.search(pattern, transcribed_text, re.IGNORECASE)
+    if match:
+        prompt = match.group(1).strip()
+        return prompt
+    else:
+        return None
+
+start_listening()
+
+
+
 '''
 prompt = input('USER:')
 function_response = function_call(prompt)
@@ -112,30 +210,5 @@ print(f'Function call: {function_response}')
 response = groq_prompt(prompt)
 print(response)
 '''
-while True:
-    prompt = input('USER: ')
-    call = function_call(prompt)
-
-    if 'take screenshot' in call:
-        print('Taking screenshot')
-        take_screenshot()
-        visual_context=vision_prompt(prompt=prompt, photo_path='screenshot.jpg')
-    elif 'capture webcam' in call:
-        print('Captuing webcam')
-        web_cam_capture()
-        visual_context = vision_prompt(prompt=prompt, photo_path='webcam.jpg')
-    elif 'extract clipboard' in call:
-        print('Copying clipboard text')
-        paste = get_clipboard_text()
-        prompt = f'{prompt}\n\n CLIPBOARD CONTENT: {paste}'
-        visual_context = None
-    else:
-        visual_context = None
-
-    response = groq_prompt(prompt=prompt, img_context=visual_context)
-    print(response)
-
-
-
 
 
