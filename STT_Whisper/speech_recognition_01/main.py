f6ae58719a3314acc7502c96d011bc1f063c8cc9
alexsevas <mralexrelax@gmail.com>
2024-10-03 17:51:16 +0300

    Add whisper example

--- STT_Whisper/speech_recognition_01/main.py
+++ STT_Whisper/speech_recognition_01/main.py
@@ -1,32 +1,15 @@
 # conda activate extras
 # pip install openai-whisper
+# p2day
 import whisper
 
-# Установим путь к модели в переменную окружения
-#os.environ['WHISPER_CACHE_DIR'] = 'C:/PROJECTS/_Weights_/Whisper'
-language = 'ru'
-#model_path = 'C:\\PROJECTS\\_Weights_\\Whisper\\large-v3.pt'
-audiofile = 'C:\\PROJECTS\\_DATA_\\audio.mp3'
-
-def speech_recognition(model='tiny', audiofile='C:\\PROJECTS\\_DATA_\\audio.mp3'):
+def speech_recognition(model='small'):
     speech_model = whisper.load_model(model)
-    options = whisper.DecodingOptions(language=language)
-
-    # Загружаем аудио файл
-    #audio = whisper.load_audio(audiofile)
-
-    # Цель метода `pad_or_trim` - обеспечить, чтобы все входные звуковые сигналы имели согласованный размер и формат,
-    # что помогает Whisper производить более точные результаты. По умолчанию Whisper использует внутреннюю частоту
-    # дискретизации 16 кГц, поэтому если ваш входной звуковой сигнал имеет другую частоту дискретизации, он будет
-    # ресемплирован до соответствующей внутренней частоты перед обработкой.
-    #audio = whisper.pad_or_trim(audio)
-
-    # Выполняем распознавание
-    result = speech_model.transcribe(audiofile)
+    whisper.DecodingOptions(language='ru')
+    result = speech_model.transcribe('C:\\PROJECTS\\_DATA_\\audio.mp3', )
 
     with open(f'transcription_{model}.txt', 'w', encoding='utf-8') as file:
         file.write(result['text'])
-
 
 def main():
     models = {1: 'tiny', 2: 'base', 3: 'small', 4: 'medium', 5: 'large'}
@@ -40,7 +23,7 @@ def main():
         raise KeyError(f'Модели {model} нет в списке!')
 
     print('Запущен процесс транскрибации, пожалуйста ожидайте...')
-    speech_recognition(model=models[model], audiofile=audiofile)
+    speech_recognition(model=models[model])
 
 
 if __name__ == '__main__':
