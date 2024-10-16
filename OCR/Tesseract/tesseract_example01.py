aa329bd703cf49a106bda2266d39baf1902000d6
alexsevas <mralexrelax@gmail.com>
2024-10-16 18:11:07 +0300

    Add tesseract_example01.py

---
+++ OCR/Tesseract/tesseract_example01.py
@@ -0,0 +1,16 @@
+#### Пример кода
+# pip install pytesseract
+
+from PIL import Image
+import pytesseract
+
+# Укажите путь к исполняемому файлу tesseract, если необходимо
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+# Открываем изображение
+image = Image.open('rov01.png')
+
+# Распознаем текст
+text = pytesseract.image_to_string(image, lang='eng')
+
+print(text)
