52b447bd877bbeda1042c6d376e2605fd0d2a57d
alexsevas <mralexrelax@gmail.com>
2024-11-05 23:19:56 +0300

    Add NERF Viser example

---
+++ NERF/Viser/examples/00_coordinate_frames.py
@@ -0,0 +1,16 @@
+"""Coordinate frames
+
+In this basic example, we visualize a set of coordinate frames.
+
+Naming for all scene nodes are hierarchical; /tree/branch, for example, is defined
+relative to /tree.
+"""
+
+import random
+import time
+
+import viser
+
+server = viser.ViserServer()
+
+
