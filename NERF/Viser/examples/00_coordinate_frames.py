442d0ebdd3ad19c50c63dcd2472e7086a33c887e
alexsevas <mralexrelax@gmail.com>
2024-11-05 23:31:29 +0300

    Add NERF Viser example

--- NERF/Viser/examples/00_coordinate_frames.py
+++ NERF/Viser/examples/00_coordinate_frames.py
@@ -13,4 +13,25 @@ import viser
 
 server = viser.ViserServer()
 
+while True:
+    # Add some coordinate frames to the scene. These will be visualized in the viewer.
+    server.scene.add_frame(
+        "/tree",
+        wxyz=(1.0, 0.0, 0.0, 0.0),
+        position=(random.random() * 2.0, 2.0, 0.2),
+    )
+    server.scene.add_frame(
+        "/tree/branch",
+        wxyz=(1.0, 0.0, 0.0, 0.0),
+        position=(random.random() * 2.0, 2.0, 0.2),
+    )
+    leaf = server.scene.add_frame(
+        "/tree/branch/leaf",
+        wxyz=(1.0, 0.0, 0.0, 0.0),
+        position=(random.random() * 2.0, 2.0, 0.2),
+    )
+    time.sleep(5.0)
 
+    # Remove the leaf node from the scene.
+    leaf.remove()
+    time.sleep(0.5)
