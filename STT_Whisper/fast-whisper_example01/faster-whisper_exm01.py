a2da6d75424288219b5c8b66a8a4389f721b96db
alexsevas <mralexrelax@gmail.com>
2024-11-08 21:41:47 +0300

    testing models faster-whisper

--- STT_Whisper/fast-whisper_example01/faster-whisper_exm01.py
+++ STT_Whisper/fast-whisper_example01/faster-whisper_exm01.py
@@ -1,20 +1,27 @@
 # pip install faster-whisper
-# conda activate whisperpy310
+# conda activate whisperpy310, extras, all2py310
 
 from faster_whisper import WhisperModel
 
 #model_size = "large-v3"
-model_size = "small"
+model_size = "base"
+
+# GPU не работает нигде
+# RuntimeError: Library cublas64_12.dll is not found or cannot be loaded
+
 
 # Run on GPU with FP16
-model = WhisperModel(model_size, device="cuda", compute_type="float16")
+#model = WhisperModel(model_size, device="cuda", compute_type="float16")
+
+# Run on CPU with FP32
+model = WhisperModel(model_size, device="cpu", compute_type="float32")
 
 # or run on GPU with INT8
-# model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
+#model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
 # or run on CPU with INT8
 # model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
-segments, info = model.transcribe("audio.mp3", beam_size=5)
+segments, info = model.transcribe("C:\\PROJECTS\\_DATA_\\audio2.wav", beam_size=5)
 
 print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
