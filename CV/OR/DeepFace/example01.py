d477513462820c16e67d7222bd29156dfc3126b6
alexsevas <mralexrelax@gmail.com>
2024-11-13 16:55:50 +0300

    Add Facial Attribute Analysis

--- CV/OR/DeepFace/example01.py
+++ CV/OR/DeepFace/example01.py
@@ -7,6 +7,9 @@
 # conda activate 3dpy39
 
 from deepface import DeepFace
+from open3d.examples.visualization.draw import actions
+
+#import dlib
 
 models = [
   "VGG-Face",
@@ -21,10 +24,36 @@ models = [
   "GhostFaceNet",
 ]
 
+#print ("dlib.DLIB_USE_CUDA = ", dlib.DLIB_USE_CUDA)
+
+#face verification
 result = DeepFace.verify(
   img1_path = "Elon_Musk.png",
   img2_path = "Elon_Musk_blue_bg.png",
   model_name = models[0],
 )
-
 print (result)
+
+'''
+#face recognition
+dfs = DeepFace.find(
+  img_path = "Elon_Musk.png",
+  db_path = "C:/workspace/my_db",
+  model_name = models[1],
+)
+print (dfs)
+
+
+#embeddings
+embedding_objs = DeepFace.represent(
+  img_path = "Elon_Musk.png",
+  model_name = models[2],
+)
+print (embedding_objs)
+'''
+# Facial Attribute Analysis
+objs = DeepFace.analyze(
+  img_path = "Putin.png",
+  actions = ['age', 'gender', 'race', 'emotion'],
+)
+print(objs)
