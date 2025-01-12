a09bf8993464821cb72f3701b34e3d31a585da85
alexsevas <mralexrelax@gmail.com>
2025-01-12 20:57:44 +0300

    Add translator_app_2.py

---
+++ miniAPP/simple_translator/translator_app_2.py
@@ -0,0 +1,16 @@
+import asyncio
+from googletrans import Translator
+
+async def translate_text():
+    async with Translator() as translator:
+        result = await translator.translate('안녕하세요.')
+        print(result)  # <Translated src=ko dest=en text=Good evening. pronunciation=Good evening.>
+        result = await translator.translate('안녕하세요.', dest='ja')
+        print(result)
+
+        result = await translator.translate('veritas lux mea', src='la', dest='en')
+        print(result)
+        result = await translator.translate('veritas lux mea', src='la', dest='ru')
+        print(result)  # <Translated src=la dest=en text=The truth is my light pronunciation=The truth is my light>
+
+asyncio.run(translate_text())
