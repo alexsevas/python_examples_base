3ae7281da4713ce356670a8ee9bb413ac986552b
alexsevas <mralexrelax@gmail.com>
2025-01-14 23:54:56 +0300

    Add pytube example2

---
+++ Video/Youtube/yt-dlp/app2.py
@@ -0,0 +1,9 @@
+# GPT
+# ENV allpy311
+
+# pip install yt-dlp
+
+from yt_dlp import YoutubeDL
+ydl_opts = {'outtmpl': 'video.mp4'}
+with YoutubeDL(ydl_opts) as ydl:
+    ydl.download(["https://www.youtube.com/watch?v=3WUnBrqB3uQ"])
