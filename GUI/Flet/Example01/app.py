976e1892379c83d390256df35836aeccd9d5ab16
alexsevas <mralexrelax@gmail.com>
2025-01-28 23:42:18 +0300

    Add flet app example

---
+++ GUI/Flet/Example01/app.py
@@ -0,0 +1,19 @@
+# ENV ytpy310
+
+# pip install flet
+
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Flet App"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    def button_click(e):
+        page.add(ft.Text(f"Привет, {name_input.value}!"))
+
+    name_input = ft.TextField(label="Введите имя", width=300)
+    greet_button = ft.ElevatedButton("Поздороваться", on_click=button_click)
+
+    page.add(name_input, greet_button)
+
+ft.app(target=main)
