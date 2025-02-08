c6128dc1eb184e7a87593158ae57ae548d21df3d
alexsevas <mralexrelax@gmail.com>
2025-02-08 12:28:15 +0300

    Add comments

--- STT_Whisper/fast-whisper_example01/faster-whisper_exm01.py
+++ STT_Whisper/fast-whisper_example01/faster-whisper_exm01.py
@@ -1,26 +1,25 @@
 # pip install faster-whisper
-# conda activate whisperpy310, extras, all2py310
+# 2025 ENV allpy310
+# OLD conda activate whisperpy310, extras, all2py310
 
 from faster_whisper import WhisperModel
 
-#model_size = "large-v3"
-#model_size = "medium"
-model_size = "base"
+model_size = "base" # "large-v3" "medium" "base" "small" "tiny"
 
-# GPU не работает нигде
-# RuntimeError: Library cublas64_12.dll is not found or cannot be loaded
-
-
-# Run on GPU with FP16
-#model = WhisperModel(model_size, device="cuda", compute_type="float16")
+# GPU не работает: RuntimeError: Library cublas64_12.dll is not found or cannot be loaded
+# т.е. требуется CUDA 12.
+# Можно попробовать поставить CUDA 12 совместно с уже установленной CUDA 11.8, и для нужных приложений в PATH оставлять
+# путь только к нужной версии CUDA - GPT расписала такой вариант, как рабочий
 
 # Run on CPU with FP32
 model = WhisperModel(model_size, device="cpu", compute_type="float32")
-
+# Run on GPU with FP16
+#model = WhisperModel(model_size, device="cuda", compute_type="float16")
 # or run on GPU with INT8
 #model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
 # or run on CPU with INT8
 # model = WhisperModel(model_size, device="cpu", compute_type="int8")
+
 
 segments, info = model.transcribe("C:\\PROJECTS\\_DATA_\\audio2.wav", beam_size=5)
 
