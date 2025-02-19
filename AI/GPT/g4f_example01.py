9d0fbfdb6d2ef474a3385af4d92281b3e3286c3b
alexsevas <mralexrelax@gmail.com>
2025-02-19 21:50:12 +0300

    Add modellist g4f example app

--- AI/GPT/g4f_example01.py
+++ AI/GPT/g4f_example01.py
@@ -1,4 +1,4 @@
-# conda activate allpy311
+# conda activate allpy311, extras2
 # pip install -U g4f
 
 #from openai import OpenAI
@@ -7,18 +7,19 @@ from g4f.client import Client
 
 def ask_gpt_free():
     client = Client()
-    content = f"Напиши подробную инструкцию, как создать портативную (portable) сборку любой нейросети c github, чтобы " \
+    '''content = f"Напиши подробную инструкцию, как создать портативную (portable) сборку любой нейросети c github, чтобы " \
               "она запускалась полностью автономно, чтобы все веса уже были заранее скачаны в папке самой портативной сборки" \
               " и чтобы для ее работы не требовалось подключение к интернету. Напиши пошаговую подробную инструкцию с подробным" \
-              " описание всех шагов, возможных проблемах при создании такой сборки и их решении. Спасибо."
-    model_list = ["gpt-4-turbo", "gpt-4o-mini", "gpt-3.5-turbo"]
+              " описание всех шагов, возможных проблемах при создании такой сборки и их решении. Спасибо."'''
+    content = f"Кто ты?"
+    model_list = ["gpt-4o", "gpt-4o-mini", "gpt-3.5-turbo"]
     for model in model_list:
         try:
             response = client.chat.completions.create(
                 model=model,
                 messages=[{"role": "user", "content": content}],
             )
-            print (f'Ответ от модели - ' + model)
+            print (f'ОТВЕТ ОТ МОДЕЛИ: ' + model)
             return response.choices[0].message.content
         except Exception as err:
             print(err)
