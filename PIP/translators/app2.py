16a9a5a97d7a83ffc15a249d49f7ee81d4ea5ce7
alexsevas <mralexrelax@gmail.com>
2025-02-22 13:40:59 +0300

    Fix translators example app

--- PIP/translators/app2.py
+++ PIP/translators/app2.py
@@ -1,9 +1,11 @@
 # ENV allpy310
-
 # pip install translators
+# pip install --upgrade translators
 
-from translators.server import translate
+
+from translators.server import translate_text  #fix - метод translate устарел
+# pip install --upgrade translators
 
 text = "Hello, how are you?"
-translated_text = translate(text, from_language="en", to_language="fr", engine="google")
+translated_text = translate_text(text, from_language="en", to_language="fr", engine="google")
 print(translated_text)  # "Bonjour, comment ça va ?"
