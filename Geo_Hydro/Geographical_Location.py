92e724733ea07560668424cf6fe9df6eeed766ea
alexsevas <mralexrelax@gmail.com>
2025-03-01 07:42:45 +0300

    Add geocoder (find location) example

---
+++ Geo_Hydro/Geographical_Location.py
@@ -0,0 +1,17 @@
+# pip install geocoder reverse_geocoder
+
+# conda activate allpy310
+
+import geocoder
+import reverse_geocoder as rg
+
+
+def location():
+    latitude, longitude = [*geocoder.ip("me").latlng]
+    city = city = rg.search((latitude, longitude), verbose=False)
+    return city[0]["name"]
+
+
+if __name__ == "__main__":
+    print("Your current location: " + location())
+
