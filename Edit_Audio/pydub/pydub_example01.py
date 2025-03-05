7ad2b63d44215b13eef674ca95db46a135a7fd07
alexsevas <mralexrelax@gmail.com>
2025-03-05 18:04:32 +0300

    Edit example edit audio with pydub

---
+++ Edit_Audio/pydub/pydub_example01.py
@@ -0,0 +1,17 @@
+# conda activate allpy310
+
+# pip install pydub
+
+from pydub import AudioSegment
+
+# Загрузка аудиофайла
+audio = AudioSegment.from_file("gde_more_sunday.mp3")
+
+# Обрезка аудио с 41-й по 56-ю секунду
+audio_cut = audio[41000:56000]
+
+# Изменение громкости на +3 дБ
+quieter_audio = audio_cut + 3
+
+# Экспорт измененного аудиофайла
+quieter_audio.export("output.mp3", format="mp3")
