4c8d06800d3a53ba652ed15510c30a814017f19b
alexsevas <mralexrelax@gmail.com>
2025-03-15 11:04:26 +0300

    Add docx2txt.py

---
+++ Office/DOCX2TXT/docx2txt.py
@@ -0,0 +1,15 @@
+# conda activate allpy310
+
+# pip install python-docx
+
+
+from docx import Document
+
+def docx_to_txt(docx_path, txt_path):
+    doc = Document(docx_path)
+    with open(txt_path, 'w', encoding='utf-8') as txt_file:
+        for para in doc.paragraphs:
+            txt_file.write(para.text + '\n')
+
+# Пример использования
+docx_to_txt('D:\\AI_DATA\\PDF_DOC_DOCX_TXT\\example.docx', 'example.txt')
