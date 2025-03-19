236a1d996f001456b3a19bdba7a752865524a30e
alexsevas <mralexrelax@gmail.com>
2025-03-19 11:57:09 +0300

    Add comments

--- AI/gpt4free/g4f_flux_image.py
+++ AI/gpt4free/g4f_flux_image.py
@@ -1,15 +1,25 @@
+# conda activate extras2
+
 from g4f.client import Client
 
 client = Client()
+'''
 response = client.images.generate(
     model="flux",
-    prompt="a white siamese cat",
+    prompt="a professional photo of beautiful young woman with blue eyes",
     response_format="url"
 )
+'''
+response = client.images.generate(
+    model="flux",
+    prompt="a professional photo of beautiful young woman with blue eyes"
+)
 
-print(f"Generated image URL: {response.data[0].url}")
+#print(f"Generated image URL: {response.data[0].url}")
 
 '''
+response_format="url"
+---------------------
 Generated image URL: https://image.pollinations.ai/prompt/a+white+siamese+cat?seed=42162458&width=1024&height=1024&
 model=flux&nologo=true&private=false&enhance=false&safe=false
 '''
