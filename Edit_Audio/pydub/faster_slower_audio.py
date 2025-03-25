eaaeca80eae880b2470432b47c91ee656cc1c60b
alexsevas <mralexrelax@gmail.com>
2025-03-25 11:15:19 +0300

    Add faster_slower_audio.py

--- Edit_Audio/pydub/faster_slower_audio.py
+++ Edit_Audio/pydub/faster_slower_audio.py
@@ -4,12 +4,11 @@
 
 from pydub import AudioSegment
 
-# Загрузка OGG-файла
-audio = AudioSegment.from_file("data/voice_bass.ogg")
+# Загрузка MP3-файла
+audio = AudioSegment.from_file("data/gde_more_sunday.mp3")
 
 # Ускорение (2x)
 faster_audio = audio.speedup(playback_speed=2.0)
-
 # Замедление (0.5x)
 slower_audio = audio.speedup(playback_speed=0.5)
 
