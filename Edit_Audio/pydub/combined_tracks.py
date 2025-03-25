f6299739721740e87a9848ea58476b3adf8f9f4b
alexsevas <mralexrelax@gmail.com>
2025-03-25 11:09:35 +0300

    Add combined_tracks.py

---
+++ Edit_Audio/pydub/combined_tracks.py
@@ -0,0 +1,18 @@
+# conda activate allpy310
+
+# pip install pydub
+
+from pydub import AudioSegment
+
+# Загрузка MP3-файла
+audio = AudioSegment.from_file("data/voice_bass.ogg")
+# Загрузка второго трека
+background = AudioSegment.from_file("data/gde_more_sunday.mp3")
+# Уменьшим громкость фоновой музыки (второго трека)
+background = background - 20
+
+# Наложим второй трек на первый (начало с 1 секунды)
+combined = audio.overlay(background, position=1000)
+
+# Экспорт сведенного трека
+combined.export("data/combined_track.mp3", format="mp3")
