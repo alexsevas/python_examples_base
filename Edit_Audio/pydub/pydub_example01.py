f113dfd94086d6a9348c047e8248a7b0ad46f9f9
alexsevas <mralexrelax@gmail.com>
2025-03-25 10:53:14 +0300

    Fix data path

--- Edit_Audio/pydub/pydub_example01.py
+++ Edit_Audio/pydub/pydub_example01.py
@@ -5,7 +5,7 @@
 from pydub import AudioSegment
 
 # Загрузка аудиофайла
-audio = AudioSegment.from_file("gde_more_sunday.mp3")
+audio = AudioSegment.from_file("data/gde_more_sunday.mp3")
 
 # Обрезка аудио с 41-й по 56-ю секунду
 audio_cut = audio[41000:56000]
@@ -14,4 +14,4 @@ audio_cut = audio[41000:56000]
 quieter_audio = audio_cut + 3
 
 # Экспорт измененного аудиофайла
-quieter_audio.export("output.mp3", format="mp3")
+quieter_audio.export("data/output.mp3", format="mp3")
