6621ba6cf81dc3978eef62ac38e24381df7a0baa
alexsevas <mralexrelax@gmail.com>
2025-03-30 05:30:10 +0300

    Add phone_details.py

---
+++ Security/_OSINT_/ghoulbond/modules/phone_details.py
@@ -0,0 +1,73 @@
+# conda activate allpy310
+
+"""Modules"""
+import phonenumbers
+from phonenumbers import carrier
+from phonenumbers import timezone
+from colorama import Fore
+
+"""Built in function for country name"""
+from phonenumbers import geocoder
+
+
+def time_zone_details(number):
+    """Provides Time zone details of the registered number"""
+    ch_number = phonenumbers.parse(number)
+
+    try:
+        timeZone = timezone.time_zones_for_number(ch_number)
+        print(Fore.RED + "TimeZone : ", end="")
+
+        for individual_time_zone in timeZone:
+            print(Fore.GREEN + f"{individual_time_zone}", end=" ")
+
+    except:
+        print(Fore.RED + "Does not seems like a valid number enter valid number")
+
+
+def service_provider_details(number):
+    """Provide service provider details"""
+    try:
+        service_number = phonenumbers.parse(number, "RO")
+        print(Fore.RED + "Service Provider : ", end="")
+        if (carrier.name_for_number(service_number, "en") != ""):
+            print(Fore.GREEN + f'{carrier.name_for_number(service_number, "en")}')
+        else:
+            print(Fore.RED + "Entered number is wrong or no details of service provider is registered")
+
+    except:
+        print(Fore.RED + "Does not seems like a valid number enter valid number")
+
+
+def country_history(number):
+    try:
+
+        """In CH ( C is for country and H is for history)"""
+        ch_number = phonenumbers.parse(number, "CH")
+
+        print(Fore.RED + f"Phone Number : {Fore.GREEN + number}")
+        print(Fore.RED + "Country : ", end="")
+
+        if ((geocoder.description_for_number(ch_number, "en")) != ""):
+            print(Fore.GREEN + geocoder.description_for_number(ch_number, "en"), )
+        else:
+            print(Fore.RED + "Entered number is wrong /country code is missing no details of country is registered")
+
+    except:
+        print(Fore.RED + "Does not seems like a valid number enter valid number")
+        exit(0)
+
+
+def get_details():
+    print()
+    number = input(Fore.BLUE + "Enter your valid number along with country code : ").replace(" ", "")
+
+    print()
+    country_history(number)
+    service_provider_details(number)
+    time_zone_details(number)
+    print(Fore.RESET)
+
+
+if __name__ == '__main__':
+    get_details()
