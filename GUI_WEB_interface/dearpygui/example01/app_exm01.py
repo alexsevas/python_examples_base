f94089599f7ac2f6b7f442a54b93b649ab656c0a
alexsevas <mralexrelax@gmail.com>
2025-03-30 18:55:12 +0300

    Add  dearpygui example

---
+++ GUI_WEB_interface/dearpygui/example01/app_exm01.py
@@ -0,0 +1,35 @@
+# conda activate allpy310
+# pip install dearpygui
+
+import dearpygui.dearpygui as dpg
+
+# Меню с выпадающим списком.
+
+dpg.create_context()
+
+with dpg.window(label="Main"):
+
+    with dpg.menu_bar():
+        with dpg.menu(label="Themes"):
+            dpg.add_menu_item(label="Dark")
+            dpg.add_menu_item(label="Light")
+            dpg.add_menu_item(label="Classic")
+
+            with dpg.menu(label="Other Themes"):
+                dpg.add_menu_item(label="Purple")
+                dpg.add_menu_item(label="Gold")
+                dpg.add_menu_item(label="Red")
+
+        with dpg.menu(label="Tools"):
+            dpg.add_menu_item(label="Show Logger")
+            dpg.add_menu_item(label="Show About")
+
+        with dpg.menu(label="Oddities"):
+            dpg.add_button(label="A Button")
+            dpg.add_simple_plot(label="Menu plot", default_value=(0.3, 2.9, 2.5, 18.9), height=100)
+
+dpg.create_viewport(title='Custom Title', width=800, height=600)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
